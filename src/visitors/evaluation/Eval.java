500b8496631969d752adb080f6d741144125e54c
rollingflamingo <andrea.storace.uni@gmail.com>
2018-07-10 15:11:45 +0200

    fixed visitList

--- src/visitors/evaluation/Eval.java
+++ src/visitors/evaluation/Eval.java
@@ -81,7 +81,7 @@ public class Eval implements Visitors<Value> {
                 }
                 break;
             case USER:
-                if(o == null) toList = broker.getConnectedgit Users();
+                if(o == null) toList = broker.getConnectedUsers();
                 else {
                     try {
                         toList = broker.getTopics().getTopicNamed(o.getName()).ListUsers();
